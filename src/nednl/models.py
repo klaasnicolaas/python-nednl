f6acc30e59856463c049c43ad25d4eaa418608d6
Klaas Schoute <klaas_schoute@hotmail.com>
2024-03-28 20:17:38 +0100

    Improve the class models (#9)

--- src/nednl/models.py
+++ src/nednl/models.py
@@ -3,55 +3,70 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
+from typing import Generic, TypeVar
 
 from mashumaro import field_options
+from mashumaro.config import BaseConfig
+from mashumaro.mixins.dict import DataClassDictMixin
 from mashumaro.mixins.orjson import DataClassORJSONMixin
 
+_ResultDataT = TypeVar("_ResultDataT")
+
 
 @dataclass
-class ActivitiesResponse(DataClassORJSONMixin):
-    """Object representing an Activities API response."""
+class NedDataMixin(DataClassDictMixin):
+    """Mixin for dataclasses with a 'data' attribute."""
 
-    data: list[Activity] = field(metadata=field_options(alias="hydra:member"))
+    # pylint: disable-next=too-few-public-methods
+    class Config(BaseConfig):
+        """Configuration for mashumaro."""
+
+        serialize_by_alias = True
+
+
+@dataclass
+class BaseResponse(Generic[_ResultDataT], NedDataMixin, DataClassORJSONMixin):
+    """Base object representing an API response."""
+
+    data: _ResultDataT = field(metadata=field_options(alias="hydra:member"))
     items: int = field(metadata=field_options(alias="hydra:totalItems"))
 
 
-@dataclass
-class GranularitiesResponse(DataClassORJSONMixin):
-    """Object representing an Granularities API response."""
-
-    data: list[Granularity] = field(metadata=field_options(alias="hydra:member"))
-    items: int = field(metadata=field_options(alias="hydra:totalItems"))
-
-
-@dataclass
-class PointsResponse(DataClassORJSONMixin):
-    """Object representing an Points API response."""
-
-    data: list[Point] = field(metadata=field_options(alias="hydra:member"))
-    items: int = field(metadata=field_options(alias="hydra:totalItems"))
-
-
-@dataclass
-class Activity(DataClassORJSONMixin):
+@dataclass(slots=True)
+class Activity(NedDataMixin, DataClassORJSONMixin):
     """Object representing an activity from National Energy Dashboard NL."""
 
     id: int  # noqa: A003, RUF100
     name: str
 
 
-@dataclass
-class Granularity(DataClassORJSONMixin):
+@dataclass(slots=True)
+class Granularity(NedDataMixin, DataClassORJSONMixin):
     """Object representing an granularity from National Energy Dashboard NL."""
 
     id: int  # noqa: A003, RUF100
     name: str
 
 
-@dataclass
-class Point(DataClassORJSONMixin):
+@dataclass(slots=True)
+class Point(NedDataMixin, DataClassORJSONMixin):
     """Object representing an area from National Energy Dashboard NL."""
 
     id: int  # noqa: A003, RUF100
     name: str
     nameshort: str
+
+
+@dataclass(slots=True)
+class ActivitiesResponse(BaseResponse[list[Activity]]):
+    """Object representing an Activities API response."""
+
+
+@dataclass(slots=True)
+class GranularitiesResponse(BaseResponse[list[Granularity]]):
+    """Object representing an Granularities API response."""
+
+
+@dataclass(slots=True)
+class PointsResponse(BaseResponse[list[Point]]):
+    """Object representing an Points API response."""
